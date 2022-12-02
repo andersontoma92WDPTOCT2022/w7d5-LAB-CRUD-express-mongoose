144fa18b870c8557154fcd6bd54e1ed8e11d5eea
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-12-02 15:20:42 -0300

    até it 3.1 purchase - post

--- routes/purchase.router.js
+++ routes/purchase.router.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import PurchaseModel from '../models/purchase.model.js';
 //
+// Iteração 3: Crie as rotas /purchases
 const purchaseRote = express.Router();
 //
+// do index.js-> app -> /purchase/...
+//
+// 3.1 Crie a rota POST /purchases
+//
+purchaseRote.post('/purchases', async (req, res) => {
+  try {
+    //
+    const newPurchase = await PurchaseModel.create({
+      ...req.body,
+    });
 
+    return res.status(201).json(newPurchase);
+    //
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 //
 export default purchaseRote;
